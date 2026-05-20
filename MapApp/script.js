93c1712f63d793bc68d987fc53614f653822b4d0
JasonKam002 <jasonkam1311@gmail.com>
2026-05-20 10:36:22 -0300

    Part 4 workout classes added

--- MapApp/script.js
+++ MapApp/script.js
@@ -11,4 +11,107 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map
+let mapEvent
 
+let workouts = []
+
+class Workout {
+    date = new Date()
+    id=(Date.now() + '').slice(-10)
+
+    constructor(coords, distance, duration) {
+        this.coords = coords
+        this.distance = distance
+        this.duration = duration
+    }
+}
+
+class Running extends Workout {
+    constructor(coords, distance, duration, cadence) {
+        super(coords, distance, duration)
+        this.cadence = cadence
+    }
+}
+
+class Cycling extends Workout {
+    constructor(coords, distance, duration, elevationGain) {
+        super(coords, distance, duration)
+        this.elevationGain = elevationGain
+    }
+}
+
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+        const latitude = position.coords.latitude
+        const longitude = position.coords.longitude
+        console.log(`https://www.google.com/maps/@${latitude},${longitude},16.05z`)
+
+        const coords = [latitude, longitude]
+        map = L.map('map').setView(coords, 13);
+
+        L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+        L.marker(coords).addTo(map)
+            .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+            .openPopup();
+
+        map.on('click', function(mapE) {
+            form.classList.remove('hidden')
+            inputDistance.focus()
+            mapEvent = mapE
+        })
+    },
+
+    function() {
+        alert("Could not get position")
+    }
+
+)
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    const type = inputType.value
+    const distance = Number(inputDistance.value)
+    const duration = Number(inputDuration.value)
+    const lat = mapEvent.latlng.lat
+    const lng = mapEvent.latlng.lng
+    let workout
+
+    if (type == 'running') {
+        const cadence = Number(inputCadence.value)
+
+        workout = new Running([lat, lng], distance, duration, cadence)
+    }
+
+    if (type == 'cycling') {
+        const elevation = Number(inputElevation.value)
+
+        workout = new Cycling([lat, lng], distance, duration, elevation)
+    }
+
+    workouts.push(workout)
+
+    form.reset()
+
+    L.marker([lat, lng]).addTo(map)
+        .bindPopup(L.popup({
+            maxWidth:250,
+            minWidth:100,
+            autoClose:false,
+            closeOnClick:false,
+            className:'running-popup',
+        }))
+        .setPopupContent('Workout')
+        .openPopup();
+
+        console.log(workouts)
+    })
+
+inputType.addEventListener('change', function(){
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden')
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden')
+})
